d5b7763023ef1fea6e71d5e3984c45dab357c80d
kelvinLin9 <kelvin80121@hotmail.com>
2022-09-27 20:16:30 +0800

    u

---
+++ 筆記/6.let&const/基本概念.js
@@ -0,0 +1,38 @@
+// var myName = 'Ray2';
+// let myName = 'Ray';
+
+// console.log(myName); 
+
+
+
+
+// var myName = 'kelvin';
+// console.log('A:', myName); // A: kelvin
+// {
+//   var myName = 'kelvin2';
+//   console.log('B:', myName); // B: kelvin2
+// }
+
+// console.log('C:', myName); // C: kelvin2
+
+
+
+
+// let myName = 'kelvin';
+// console.log('A:', myName); 
+// {
+//   var myName = 'kelvin2';
+//   console.log('B:', myName); 
+// }
+
+// console.log('C:', myName); 
+
+
+var myName = 'kelvin';
+console.log('A:', myName); 
+{
+  let myName = 'kelvin2';
+  console.log('B:', myName); 
+}
+
+console.log('C:', myName); 
