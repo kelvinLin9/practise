cb9ecf333f9ae2c6231c9a465f1a1812768a3ce7
kelvinLin9 <kelvin80121@hotmail.com>
2022-10-25 19:27:53 +0800

    u

---
+++ farmProduce/farmProduce.js
@@ -0,0 +1,45 @@
+//取得資料
+
+let data = [];
+
+function getData() {
+  axios
+    .get("https://hexschool.github.io/js-filter-data/data.json")
+    .then((res) => {
+      console.log(res.data[50]["種類代碼"])
+      data = res.data.filter((i) => i["種類代碼"] && i["作物名稱"].trim())
+      data = data.filter((i) => i["交易量"] !== 0)
+      render(data)
+    });
+}
+getData();
+
+const showList = document.querySelector('.showList')
+const buttonGroup = document.querySelector('.button-group')
+
+function render(showData) {
+  let str = ''
+  showData.forEach((item) => {
+    str += `
+    <tr>
+      <td>${item["作物名稱"]}</td>
+      <td>${item["市場名稱"]}</td>
+      <td>${item["上價"]}</td>
+      <td>${item["中價"]}</td>
+      <td>${item["下價"]}</td>
+      <td>${item["平均價"]}</td>
+      <td>${item["交易量"]}</td>
+    </tr>
+    `
+  })
+  showList.innerHTML = str
+}
+
+buttonGroup.addEventListener('click',(e) => {
+  console.log(e.target.getAttribute('data-type'))
+  showData = data.filter((i) => {
+    return i["種類代碼"] == e.target.getAttribute('data-type')
+  })
+  console.log(showData)
+  render(showData)
+})
