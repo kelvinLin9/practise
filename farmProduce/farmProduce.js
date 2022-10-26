56f912a657c2b79f8ce9b6bf318b188ba58df2de
kelvinLin9 <kelvin80121@hotmail.com>
2022-10-26 19:10:50 +0800

    u

--- farmProduce/farmProduce.js
+++ farmProduce/farmProduce.js
@@ -1,6 +1,7 @@
 //取得資料
 
-let data = [];
+let data = []
+let showData = []
 let type = ''
 let sort = ''
 let upDown = ''
@@ -9,7 +10,6 @@ function getData() {
   axios
     .get("https://hexschool.github.io/js-filter-data/data.json")
     .then((res) => {
-      console.log(res.data[50]["種類代碼"])
       data = res.data.filter((i) => i["種類代碼"] && i["作物名稱"].trim())
       data = data.filter((i) => i["交易量"] !== 0)
       render(data)
@@ -18,12 +18,16 @@ function getData() {
 getData();
 
 const showList = document.querySelector('.showList')
+const showResult = document.querySelector('.show-result')
 const buttonGroup = document.querySelector('.button-group')
+const buttonGroups = document.querySelectorAll('.button-group button')
 const searchInput = document.querySelector('.rounded-end')
 const searchBtn = document.querySelector('.search')
 const sortSelect = document.querySelector('.sort-select')
 const sortAdvanced = document.querySelector('.js-sort-advanced')
-console.log(sortAdvanced)
+const sortBtn = document.querySelectorAll('.js-sort-advanced i')
+console.log(sortAdvanced.classList)
+console.log(sortBtn[0].classList)
 
 function render(showData) {
   let str = ''
@@ -41,14 +45,19 @@ function render(showData) {
     `
   })
   showList.innerHTML = str
+  showResult.innerHTML = `共${showData.length}筆資料`
 }
 
 buttonGroup.addEventListener('click',(e) => {
   type = e.target.getAttribute('data-type') 
+  buttonGroups.forEach((i) => {
+    i.classList.remove('active')
+  })
+  e.target.classList.add('active')
+  console.log(e.target.classList)
   showData = data.filter((i) => {
     return i["種類代碼"] == e.target.getAttribute('data-type')
   })
-  console.log(showData)
   render(showData)
 })
 
@@ -66,19 +75,31 @@ searchBtn.addEventListener('click', (e) => {
 
 sortSelect.addEventListener('change',(e) => {
   sort = e.target.value
-  // console.log(e.target.childNodes.getAttribute('data-sort')) 想辦法
+  upDown = 'up'
+  sortData()
 })
 
 sortAdvanced.addEventListener('click',(e) => {
   upDown = e.target.getAttribute('data-sort')
   sort = e.target.getAttribute('data-price')
-  data.sort((a,b) => {
+  sortBtn.forEach((i) => {
+    i.classList.remove('text-danger')
+  })
+  e.target.classList.add('text-danger')
+  sortData()
+})
+
+function sortData() {
+  console.log(showData)
+  if (showData === []) {
+    showData = data
+  }
+  showData.sort((a,b) => {
     if(upDown === 'up'){
       return b[sort] - a[sort]
     } else if(upDown === 'down'){
       return a[sort] - b[sort]
     }
-
   }) 
-  render(data)
-})
+  render(showData)
+}
