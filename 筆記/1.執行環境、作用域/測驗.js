865afa4ac678056622701106228245c013a3bce1
kelvinLin9 <kelvin80121@hotmail.com>
2023-01-17 13:19:06 +0800

    update

--- 筆記/1.執行環境、作用域/測驗.js
+++ 筆記/1.執行環境、作用域/測驗.js
@@ -1,15 +1,21 @@
-function sayHi (a){
-  console.log(a);
-  var a = 'Mark';
-  console.log(a);
-  function fu() {
-    var a = 'Casper';
-    console.log(a);
-  }
-  fu();
-  a = 'HexSchool'
-  console.log(a);
-}
-var a = 'Mary';
-sayHi(a);
-console.log(a);
+// function sayHi (a){
+//   console.log(a);
+//   var a = 'Mark';
+//   console.log(a);
+//   function fu() {
+//     var a = 'Casper';
+//     console.log(a);
+//   }
+//   fu();
+//   a = 'HexSchool'
+//   console.log(a);
+// }
+// var a = 'Mary';
+// sayHi(a);
+// console.log(a);
+
+
+'' = true;
+true = '';
+null = '';
+123 = ture;
