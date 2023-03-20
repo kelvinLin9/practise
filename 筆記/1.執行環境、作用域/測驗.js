1f02b5c07ad6a1db809cfe9ee025bce03f0fecbc
kelvinLin9 <kelvin80121@hotmail.com>
2023-03-20 20:57:20 +0800

    u

--- 筆記/1.執行環境、作用域/測驗.js
+++ 筆記/1.執行環境、作用域/測驗.js
@@ -34,3 +34,113 @@
 
 // a();
 
+// function sayHi(name) {
+//   var greeting = 'hi';
+//   return greeting + ' ' + name
+//  }
+//  function doSomething() {
+//   var mom = '媽媽';
+//   console.log(1, sayHi(mom));
+//  }
+ 
+//  debugger
+//  doSomething();
+
+// function sayHi(name) {
+//   var greeting = 'hi';
+//   return greeting + ' ' + name
+// }
+// function doSomething() {
+//   var mom = '媽媽';
+//   for (var i = 0; i < 3; i++) {
+//     var test = 0
+//     test ++
+//     console.log(i,test, sayHi(mom));
+//   }
+// }
+
+// debugger
+// doSomething();
+
+// 想請問當碰到函式內部有函式的情況
+// 會是一次全部創造好再執行
+// 還是創造執行創造執行呢
+
+
+//-----
+// var count = 1
+// function add(count) {
+//   count ++
+//   console.log(count)
+// }
+// function a() {
+//   console.log(count)
+//   add(count)
+// }
+
+// debugger
+// a() // 1 2
+// a() // 1 2
+
+/*
+1.創造階段
+函式優先
+function add(count) {
+  count ++
+  console.log(count)
+}
+function a() {
+  console.log(count)
+  add(count)
+}
+var count
+
+2.執行階段
+count = 1
+a() // 函式內沒有變數count，所以根據範圍鏈會向外層(全域)找到count，console.log(count)會印出1，然後執行add(count)
+add(count) // 由於有傳入參數，count = 1，接著執行count++，印出2
+離開add(count)的執行環境(釋放記憶體)
+離開a()的執行環境(釋放記憶體)
+再次執行a()並重複上述動作
+
+想請問助教，兩次執行a()都印出12，是因為傳入參數所以在函式add的執行環境中另外宣告了一個變數count的關係嗎
+*/
+
+
+
+
+// 解決辦法 閉包
+
+
+
+function a() {
+  var count = 1
+  function add() {
+    count ++
+    console.log(count)
+  }
+  return add
+}
+
+var test = a()
+debugger
+test()
+test()
+
+/*
+1. 創造階段
+function a() {
+  var count = 1
+  function add() {
+    count ++
+    console.log(count)
+  }
+  return add
+}
+var test 
+
+2. 執行階段
+test = a()
+test()
+
+*/
--- 筆記/5.this/all.js
+++ 筆記/5.this/all.js
@@ -1,18 +1,150 @@
-console.log(123)
+// console.log(123)
 
-function qq () {
-  let a = 'MVP'
-  console.log(this)
-  console.log(this.a)
-}
-
-qq()
-
-console.log(x)
-
-
-// function fn () {
-
+// function qq () {
+//   let a = 'MVP'
+//   console.log(this)
+//   console.log(this.a)
 // }
 
-// fn();
+// qq()
+
+// console.log(x)
+
+
+// var name = '全域'
+
+// function fn () {
+//   console.log(this.name)
+// }
+
+// obj = {
+//   name: 'FQ',
+//   fn,
+//   fn2: function () {
+//     console.log(this.name)
+//     return this.name
+//   }
+// }
+// var t = obj.fn2
+// var t2 = obj.fn2()
+// fn()
+// obj.fn();
+// obj.fn2();
+// console.log(t())
+// console.log(t2)
+
+
+// var arr = [1, 2, 3];
+// console.log(arr.length);
+// console.log(arr.unshift(555));
+// console.log(arr);
+
+
+// function Animal(family) {
+//   this.kingdom = '動物界';
+//   this.family = family || '人科'; // 科別
+// }
+
+// Animal.prototype.breathe = function() {
+//   console.log(this.name + ' 正在持續呼吸');
+// }
+
+// function Dog(name, color, size) {
+//   Animal.call(this, '犬科')
+//   this.name = name; // 狗的名字
+//   this.color = color; // 狗的顏色
+//   this.size = size; // 狗的體型
+// }
+
+// Dog.prototype = Object.create(Animal.prototype);
+// Dog.prototype.constructor = Dog;
+
+// Dog.prototype.back = function() {
+//   console.log(this.name + ' 吠叫');
+// }
+
+// var bibi = new Dog('bibi', '棕色', '小');
+// console.log(bibi.constructor); // Dog 的建構函式
+// console.log(bibi); // Dog 的建構函式
+// console.log(Animal); // Dog 的建構函式
+
+
+// function test(v){
+//   console.log(v)
+//   console.log(vv)
+//   var v = 3
+//   console.log(v)
+// }
+// test()
+
+
+
+// var nameA = "Microsoft";
+ 
+// function funcA(){
+//     var nameA = "Google";
+//     console.log(nameA);
+// }
+// console.dir(funcA); 
+// funcA(); //Google
+// console.log(nameA); //Microsoft
+
+
+
+
+
+// ----------------------
+// var nameK = "Microsoft";
+ 
+// function funcA(){
+//   var nameK = "Google";
+//   alert(nameK);
+//   return function(){
+//       nameK = "Facebook";
+//       alert(nameK);
+//   };
+// }
+
+// var o = funcA(); //Google
+// console.dir(o)
+// // alert(nameK); //Microsoft
+// o(); //Facebook
+
+
+
+// function complex(num) {
+//   // 複雜計算
+//   console.log('calculate'); // 通過這行就可以知道有沒有經過計算
+//   return num * num * num;
+// }
+// function cache(func) {
+//   var ans = 0;
+//   return function(num) {
+//   // 因為簡化語法，所以可以直接 return 這個 function
+//     if (ans) {
+//       return ans;
+//     }
+    
+//     ans = func(num); // 在這裡等同於 ans[20] = complex(20)
+//     return ans;
+//   }
+// }
+// const cachedComplex = cache(complex)
+// console.log(cachedComplex(20)) // 計算
+// console.log(cachedComplex(20)) // 純輸出，就會判斷 ans[20] 存在否？
+// console.log(cachedComplex(20)) // 純輸出
+
+
+var a = 1
+function test() {
+  var b = 2
+  function inner() {
+    var c = 3
+    console.log(b)
+    console.log(a)
+  }
+  inner()
+}
+test()
+console.dir(test)
+console.dir(window)
