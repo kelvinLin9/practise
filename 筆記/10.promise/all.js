ee15ae037dbea5563fde7983951da31f645cf905
kelvinLin9 <kelvin80121@hotmail.com>
2023-09-23 19:39:58 +0800

    u

--- 筆記/10.promise/all.js
+++ 筆記/10.promise/all.js
@@ -11,5 +11,51 @@ function promiseEx() {
     reject('失敗')
   });
 }
-
+console.dir(promiseEx);
 console.log(promiseEx());
+
+
+function promiseCook (foodName, time) {
+  return new Promise((resolve, reject) => {
+    if (time >= 3 && time <= 5) {
+      resolve(`${foodName}泡了${time}分鐘，好吃`)
+    } else {
+      reject(`${foodName}泡了${time}分鐘，難吃`)
+    }
+  })
+}
+
+// console.dir(promiseCook)
+// console.log(promiseCook('來一客' , 3))
+
+
+
+// const cookTime = parseInt(Math.random() * 7) // 隨機帶入分鐘
+
+// promiseCook('來一客' , cookTime)
+// .then((res) => {
+//   console.log(res)
+// })
+// .catch((err) => {
+//   console.log(err)
+// })
+
+
+// 鏈接不同promise
+promiseCook('來一客' , 3)
+  .then((res) => {
+    console.log(res)
+    return promiseCook('再來一客' , 4)
+  })
+  .then((res) => {
+    console.log(res)
+    return promiseCook('又來一客' , 40) // 這個階段會進入 catch
+  })
+  .then((res) => {
+    console.log(res)
+    return promiseCook('還來一客' , 5) // 由於上一個階段結果是 reject，所以此段不執行
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
